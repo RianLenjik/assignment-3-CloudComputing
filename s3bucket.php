4c05c8f0ae6fac8c928055f591d943f8d96c61c9
RianLenjik <48457236+RianLenjik@users.noreply.github.com>
2021-06-14 18:27:43 +0800

    Deleted my AWS credentials

--- s3bucket.php
+++ s3bucket.php
@@ -9,7 +9,7 @@ $s3Client = new S3Client([
     'version' => 'latest',
     'region' => 'us-east-1',
     'credentials' => [
-        'key' => 'AKIAZ5GJJKV6WO6QCCVN',
-        'secret' => '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'
+        'key' => '',
+        'secret' => ''
     ],
 ]);
